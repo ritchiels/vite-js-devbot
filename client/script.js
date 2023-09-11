e4cfe34e0f32ea10d7a032f7e0b042f004f169fc
Ritchie Simmons <ritchiels.alt@gmail.com>
2023-09-10 21:36:43 -0400

    updated server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     //fetch data from server -> bot response
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://dev-bot-wbco.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
